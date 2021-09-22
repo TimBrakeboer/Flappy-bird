bfbf154176d2f55212f3bd59c933223e390ed010
Tim Brakeboer <67371478+TimBrakeboer@users.noreply.github.com>
2021-09-22 09:31:09 +0000

    Figuur van boven naar beneden laten bewegen

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var x, y, vx, vy;
 
 function setup() {
-	createCanvas(500, 400);
+  createCanvas(500, 400);
 
   x = 200;
   y = 200;
@@ -10,16 +10,16 @@ function setup() {
 }
 
 function draw() {
-	background(0);
-  
- stroke('white');
- fill('red');
- rect(x,y,40,40);
- strokeWeight(5)
+  background(0);
+
+  stroke('white');
+  fill('red');
+  rect(x, y, 40, 40);
+  strokeWeight(5)
   y = y + vy;
 
 
-  if(y < 0 || y > 400){
+  if (y < 0 || y > 400) {
     vy = vy * -1;
   }
 }

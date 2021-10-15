cc0f591d4eaa1c72322dff2426a2cb2b42e8cc38
Tim Brakeboer <67371478+TimBrakeboer@users.noreply.github.com>
2021-10-15 10:02:37 +0000

    Vogel toegevoegd

--- script.js
+++ script.js
@@ -1,25 +1,106 @@
-var x, y, vx, vy;
+var birb, pipe, pipe2;
+var pipes = [];
+var bg, birb_img;
 
-function setup() {
-  createCanvas(500, 400);
+class Birb {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vy = 0;
+    this.w = 25;
+    this.h = 25;
+    this.gravity = 0.2;
+  }
 
-  x = 200;
-  y = 200;
-  vx = 5;
-  vy = 5;
+  //Cirkel maken
+  drawBirb() {
+    //Zwaartekracht
+    this.vy += this.gravity;
+
+    this.y += this.vy;
+
+    //Niet door de grond
+    if (this.y > 340) {
+      this.vy = 0;
+      this.y = 340;
+    }
+    //Niet door het dak
+    if (this.y < 0) {
+      this.vy = 0;
+      this.y = 0;
+    }
+    image(birb_bg, this.x, this.y, this.w, this.h);
+  }
 }
 
+
+class Pipe {
+  constructor(x, y, h) {
+    this.x = x;
+    this.y = y;
+    this.h = h;
+    this.w = 50;
+    this.c = "green";
+  }
+
+  drawPipe() {
+    fill(this.c)
+    rect(this.x, this.y, this.w, this.h);
+    this.x = this.x - 5;
+  }
+  //buis rood bij birb = pipe
+  checkCollision() {
+    if (birb.x < this.x + this.w && birb.x + birb.w > this.x) {
+      if (birb.y < this.y + this.h && birb.y + birb.h > this.y) {
+        this.c = "red";
+      }
+    }
+    else {
+      this.c = "green";
+    }
+  }
+}
+
+//achtergrond
+function preload() {
+  bg = loadImage('bg.png');
+  birb_bg = loadImage('birb.png');
+}
+
+//Grootte van speelveld en positie figuur
+function setup() {
+  createCanvas(580, 360);
+
+  birb = new Birb(100, 200);
+
+  console.log(pipes);
+}
+
+
 function draw() {
-  background(0);
-
-  stroke('white');
-  fill('red');
-  rect(x, y, 40, 40);
-  strokeWeight(5)
-  y = y + vy;
+  image(bg, 0, 0, width, height);
 
 
-  if (y < 0 || y > 400) {
-    vy = vy * -1;
+  if (frameCount % 60 == 0) {
+    //console.log("draw pipe!");
+
+    let randomHeight = random(height - 150)
+
+    pipes.push(new Pipe(800, 0, randomHeight));
+    pipes.push(new Pipe(800, randomHeight + 100, 1000));
+  }
+
+  birb.drawBirb();
+
+  pipes.forEach((p) => {
+    p.drawPipe();
+    p.checkCollision();
+  });
+}
+
+//bestuurbaar maken
+function keyPressed() {
+  if (keyCode == 32) {
+    birb.vy -= 5;
   }
 }
